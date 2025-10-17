36f3867b22fef3398e7dd2e3c40894fa5591f867
17decky <deckysetyawan@gmail.com>
2025-10-17 16:14:28 +0700

    edit endpoint service

--- service/flaskr/hello.py
+++ service/flaskr/hello.py
@@ -1,12 +1,32 @@
-import os
-
-from flask import Flask
-
-from flask import Flask
+import socket
+from flask import Flask, jsonify
+from functools import wraps
 
 app = Flask(__name__)
 
+# --- Custom decorator to return JSON ---
+def json_response(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        return jsonify(result)
+    return wrapper
 
+
+# --- Endpoint 1: Hello World ---
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
+
+# --- Endpoint 2: Hostname ---
+@app.route('/hostname')
+@json_response
+def get_hostname():
+    hostname = socket.gethostname()
+    return {"hostname": hostname}
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
