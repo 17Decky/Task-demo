ddf0f21389bcacb245d21f37c4cbfa596eced763
17decky <deckysetyawan@gmail.com>
2025-10-20 05:44:34 +0700

    adjustment keda scalled object

--- service/flaskr/hello.py
+++ service/flaskr/hello.py
@@ -1,8 +1,29 @@
 import socket
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, Response
 from functools import wraps
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
 
 app = Flask(__name__)
+
+# --- Prometheus metrics ---
+REQUEST_COUNTER = Counter(
+    'http_requests_total', 'Total HTTP Requests', ['method', 'endpoint', 'http_status']
+)
+
+# --- Middleware to count requests ---
+@app.before_request
+def before_request():
+    request._start_path = request.path  # Save path to use in after_request
+
+
+@app.after_request
+def after_request(response):
+    REQUEST_COUNTER.labels(
+        method=request.method,
+        endpoint=request._start_path,
+        http_status=response.status_code
+    ).inc()
+    return response
 
 # --- Custom decorator to return JSON ---
 def json_response(func):
@@ -25,6 +46,11 @@ def hello():
 def get_hostname():
     hostname = socket.gethostname()
     return {"hostname": hostname}
+
+# --- Prometheus metrics endpoint ---
+@app.route('/metrics')
+def metrics():
+    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
 
 
 if __name__ == '__main__':
